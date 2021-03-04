be2c018bd90b5ac9bf46a631fc47edaab6c5c7b0
Erica Roquemore <enroquemore@gmail.com>
2021-03-04 13:19:25 -0500

    added comments

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-// Assignment Code
 debugger;
+// Assignment Code
 var generateBtn = document.querySelector("#genenrate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Generate Password Function
+
 function generatePassword() {
   var lengthOfPassword = prompt(
   "Select a length between 8 and 128 characters"
 ); 
 
 if (lengthOfPassword >= 8 && lengthOfPassword <= 128) {
+  // Alerts
   var resultSpecial = confirm("Click ok to confirm special characters");
   var resultNumeric = confirm("Click ok to confirm numeric characters");
   var resultLower = confirm("Click ok to confirm lowercase characters");
-  var resultUpper = confirm("Click ok to confirm uppercase characters")
+  var resultUpper = confirm("Click ok to confirm uppercase characters");
 } else {
   alert(
     "Password length must be a length of at least 8 characters and no more than 128 characters"
@@ -83,29 +99,19 @@ function getNumber() {
 
 
 
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-
-
-
-
-
-
-
-
