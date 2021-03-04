f7d2647bd7d1650c5344a086b30654581ca98118
Erica Roquemore <enroquemore@gmail.com>
2021-03-04 11:20:52 -0500

    updated javascript

--- script.js
+++ script.js
@@ -2,9 +2,28 @@
 debugger;
 var generateBtn = document.querySelector("#genenrate");
 function generatePassword() {
-    //Hello World Test 
-return "Hello World";
+  var lengthOfPassword = prompt(
+  "Select a length between 8 and 128 characters"
+); 
+
+if (lengthOfPassword >= 8 && lengthOfPassword <= 128) {
+  var resultSpecial = confirm("Click ok to confirm special characters");
+  var resultNumeric = confirm("Click ok to confirm numeric characters");
+  var resultLower = confirm("Click ok to confirm lowercase characters");
+  var resultUpper = confirm("Click ok to confirm uppercase characters")
+} else {
+  alert(
+    "Password length must be a length of at least 8 characters and no more than 128 characters"
+  );
+  return "";
 }
+    
+if (resultSpecial || resultNumeric || resultLower || resultUpper) {
+} else {
+  alert("Must have at least 1 special character");
+  return "";
+}
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
